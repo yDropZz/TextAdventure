ac9239e6aff80b8df6859dc5f7b72b8966dd8302
yDropZz <ydropzz@gmail.com>
2025-09-08 12:36:50 +0200

    Some new clean default ui

---
+++ TextAdventure/UIMixup.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+// Base stuff here
+// There's exactly 209 - signs in a row on school computer
+// Using 20 spaces to offset to right
+
+//---------- Row 1 ------------
+
+Console.Write("                    ");
+Console.Write("Player");
+Console.Write("                    ");
+//--------- Row 2 -------------
+Console.WriteLine("");
+Console.Write("                  ");
+Console.Write("⸕ Mining: 23 (15/35)⸕");
+Console.Write("                  ");
+Console.Write("✎ Woodcutting: 51 (48/108)✎");
+Console.Write("                  ");
+Console.Write("❦ Melee: 10 (12/32)❦");
+Console.Write("                  ");
+Console.Write("✯ Magic: 12 (14/38)✯");
+Console.Write("                  ");
+Console.WriteLine("↠ Range: 21 (37/56)↠");
+Console.WriteLine("-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------");
+Console.Write("                    ");
+Console.Write("");
+
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
+Console.WriteLine("");
